efaa497c2a6736a00d8a9bfceec8157f492dfec6
Enzo <enzo.maros@etudiant.univ-rennes1.fr>
2022-04-09 23:23:34 +0200

    Récupération config depuis branche "construction"

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'orange': '#DB5A42',
+      'black': '#4D4B44',
+      'white': '#F2F2F2'
+    },
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'ubuntu': ['Ubuntu', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
