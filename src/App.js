abb82a9a4df902aaf4b6b0434753a8f5d0b3c4d9
Enzo <enzo.maros@etudiant.univ-rennes1.fr>
2022-04-09 17:14:42 +0200

    Contenu principal App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <></>
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <h1 className='text-6xl mb-4'>Site en construction<span className='text-orange'>.</span></h1>
+      <p>Portfolio actuellement en cours de développement. Disponible fin avril.</p>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
+    colors: {
+      'orange': '#DB5A42',
+      'black': '#4D4B44',
+    },
     extend: {},
   },
   plugins: [],
