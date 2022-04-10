c030260412cf200db953072705adf1c2072659fc
Enzo <enzo.maros@etudiant.univ-rennes1.fr>
2022-04-10 12:14:14 +0200

    Changement taille NavBar lors du scroll

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,11 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
 import NavBarTitre from "./NavBarTitle";
 import NavBarMenu from "./NavBarMenu";
 import NavBarSocial from "./NavBarSocial";
 
 // Menu principal
 function NavBar() {
+  // La barre réduit dès que l'utilisateur commence à scroll
+  const [taille, setTaille] = useState("h-24");
+
+  const handleScroll = () => {
+    setTaille(window.scrollY > 0 ? "h-16" : "h-24");
+  }
+
+  useEffect(() => {
+    // Lors du montage, on initialise l'eventListener
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className="w-full px-20 py-7 flex justify-between items-center bg-[#fff] shadow-md">
+    <nav className={"fixed z-50 w-full px-20 transition-all flex justify-between items-center bg-[#fff] shadow-md " + taille}>
       <NavBarTitre>Portfolio</NavBarTitre>
       <NavBarMenu/>
       <NavBarSocial/>
