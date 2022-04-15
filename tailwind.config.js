d3a75989a73db6d261ead027037eec206bfb79d3
Enzo <enzo.maros@etudiant.univ-rennes1.fr>
2022-04-15 10:59:39 +0200

    Initialisation TailwindCSS
    Initialisation du module TailwindCSS
    Suppression des styles et contenus inutiles
    Récupération de la configuration depuis la branche main

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    colors: {
+      'orange': '#DB5A42',
+      'black': '#4D4B44',
+      'white': '#F2F2F2',
+      'grey': '#A9A79E'
+    },
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'ubuntu': ['Ubuntu', 'sans-serif']
+      },
+      borderWidth: {
+        '6': '6px'
+      }
+    },
+  },
+  plugins: [],
+}
