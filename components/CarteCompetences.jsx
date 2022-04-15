aae7ce9d9185371c0b59ec8c4ea3168febe45c66
Enzo <enzo.maros@etudiant.univ-rennes1.fr>
2022-04-15 19:17:22 +0200

    Couleur orange sur le premier mot de la carte

--- components/CarteCompetences.jsx
+++ components/CarteCompetences.jsx
@@ -2,9 +2,16 @@ import PropTypes from 'prop-types'
 
 export default function CarteCompetences(props) {
   console.log(props.children);
+
+  // Le premier mot apparaît en orange, le reste en gris
+  const [mot1, ...mots] = props.titre.split(" ")
+
   return (
     <div className='bg-[#fff] p-8 rounded-3xl flex-grow flex-shrink basis-0 shadow-lg'>
-      <h3 className='font-ubuntu font-bold text-3xl mb-8'>{props.titre}</h3>
+      <h3 className='font-ubuntu font-bold text-3xl mb-8'>
+        <span className='text-orange'>{mot1} </span>
+        {mots}
+      </h3>
       <div className='flex flex-wrap gap-10 justify-center'>
         {props.children?.map(el => {
           const IconType = el.type;
@@ -17,5 +24,4 @@ export default function CarteCompetences(props) {
 CarteCompetences.propTypes = {
   /** Titre de la carte */
   titre: PropTypes.string.isRequired,
-  /**  */
 }
