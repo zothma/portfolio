eea4826901d721f67a14150c07297b3dc0e2aee2
Enzo <enzo.maros@gmail.com>
2022-04-23 18:38:19 +0200

    Support des liens vers GitHub

--- components/Bouton.jsx
+++ components/Bouton.jsx
@@ -1,19 +1,25 @@
 import PropTypes from 'prop-types'
-import IconeDL from './Icons/Download'
-import IconeExterne from './Icons/ExternalLink'
+import { Download, ExternalLink, Github } from "./Icons"
 
 export const TypesBtn = {
   telechargement: Symbol("téléchargement"),
-  externe: Symbol("externe")
+  externe: Symbol("externe"),
+  github: Symbol("github")
 };
 
 export default function Bouton(props) {
-  const Icone = props.type === TypesBtn.externe ? IconeExterne : IconeDL;
+  const icons = {
+    [TypesBtn.telechargement] : Download,
+    [TypesBtn.externe] : ExternalLink,
+    [TypesBtn.github] : Github
+  };
+
+  const Icone = icons[props.type];
 
   return (
     <a href={props.url} className='inline-flex items-center h-12 font-ubuntu border-4 rounded-full px-5 border-orange text-orange'>
       {<Icone className='h-6 w-6 mr-3 fill-orange' />}
-      {props.texte}
+      {props.type === TypesBtn.github ? "Ouvrir sur GitHub" : props.texte}
     </a>
   )
 };
@@ -26,7 +32,7 @@ Bouton.propTypes = {
   /** URL associée au lien */
   url: PropTypes.string.isRequired,
   /** Texte affiché */
-  texte: PropTypes.string.isRequired,
+  texte: PropTypes.string,
   /** Type de bouton */
   type: PropTypes.symbol
 }
