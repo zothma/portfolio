f31213830a226b0b891591279570b270560095ca
Enzo <enzo.maros@gmail.com>
2022-04-23 17:41:07 +0200

    Ordre antéchronologique des projets

--- components/Contenu/SectionProjets.jsx
+++ components/Contenu/SectionProjets.jsx
@@ -6,48 +6,6 @@ import Projet from "../Projets/Projet";
 export default function SectionProjets() {
   const projData = [
     {
-      titre: "Implémentation du jeu de Yams",
-      icon: Gamepad,
-      image: LocalImage.Yams,
-      fond: "bg-black",
-      texte: "text-white fill-white",
-      dates: "Octobre - Novembre 2021",
-      temps: 20,
-      equipe: false,
-      description: "Premier projet durant ma formation en BUT Informatique, cette <strong>application terminal</strong> " +
-        "permet à deux joueurs de se défier sur une <strong>partie de Yams</strong>, célèbre jeu de 5 dés.",
-      competences: [
-        "extraire des besoins",
-        "implémenter",
-        "documenter",
-        "tester"
-      ],
-      technologies: [
-        Logos.C
-      ]
-    },
-
-    {
-      titre: "Fabrik Ta Pépite",
-      icon: Lightbulb,
-      image: LocalImage.FabrikTaPepite,
-      fond: "bg-[#fff]",
-      dates: "Novembre 2021 - Février 2022",
-      equipe: true,
-      texteEquipe: "Concours entrepreneurial en équipe",
-      description: "Dans le cadre du <strong>concours entrepreneurial</strong>, et par groupe de jeunes étudiants, nous " +
-        "avions 3 mois pour trouver une <strong>idée d'entreprise innovante</strong> et la développer. " +
-        "Notre équipe a eu pour idée de créer une <strong>application anti-tabac</strong>, Fuzway, dont l'innovation " +
-        "reposait sur la <strong>communication</strong> entre fumeurs et sur <strong>l'esprit d'équipe</strong>.",
-      competences: [
-        "travailler en équipe",
-        "faire preuve de créativité",
-        "anticiper",
-        "défendre un projet"
-      ]
-    },
-
-    {
       titre: "Site vitrine - KohFrais",
       icon: Global,
       image: LocalImage.KohFrais,
@@ -72,7 +30,49 @@ export default function SectionProjets() {
         Logos.Figma,
         Logos.Git
       ]
-    }
+    },
+
+    {
+      titre: "Fabrik Ta Pépite",
+      icon: Lightbulb,
+      image: LocalImage.FabrikTaPepite,
+      fond: "bg-[#fff]",
+      dates: "Novembre 2021 - Février 2022",
+      equipe: true,
+      texteEquipe: "Concours entrepreneurial en équipe",
+      description: "Dans le cadre du <strong>concours entrepreneurial</strong>, et par groupe de jeunes étudiants, nous " +
+        "avions 3 mois pour trouver une <strong>idée d'entreprise innovante</strong> et la développer. " +
+        "Notre équipe a eu pour idée de créer une <strong>application anti-tabac</strong>, Fuzway, dont l'innovation " +
+        "reposait sur la <strong>communication</strong> entre fumeurs et sur <strong>l'esprit d'équipe</strong>.",
+      competences: [
+        "travailler en équipe",
+        "faire preuve de créativité",
+        "anticiper",
+        "défendre un projet"
+      ]
+    },
+    
+    {
+      titre: "Implémentation du jeu de Yams",
+      icon: Gamepad,
+      image: LocalImage.Yams,
+      fond: "bg-black",
+      texte: "text-white fill-white",
+      dates: "Octobre - Novembre 2021",
+      temps: 20,
+      equipe: false,
+      description: "Premier projet durant ma formation en BUT Informatique, cette <strong>application terminal</strong> " +
+        "permet à deux joueurs de se défier sur une <strong>partie de Yams</strong>, célèbre jeu de 5 dés.",
+      competences: [
+        "extraire des besoins",
+        "implémenter",
+        "documenter",
+        "tester"
+      ],
+      technologies: [
+        Logos.C
+      ]
+    },
   ]
 
   return (
