a8d7cb4e57b0d785e91730b26997521bf6ff4871
Enzo <enzo.maros@gmail.com>
2022-04-23 15:46:02 +0200

    Contenu et mise en forme principale projets

--- components/Contenu/SectionProjets.jsx
+++ components/Contenu/SectionProjets.jsx
@@ -13,8 +13,8 @@ export default function SectionProjets() {
       dates: "Octobre - Novembre 2021",
       temps: 20,
       equipe: false,
-      description: "Premier projet durant ma formation en BUT Informatique, cette <b>application terminal</b> " +
-        "permet à deux joueurs de se défier sur une <b>partie de Yams</b>, célèbre jeu de 5 dés.",
+      description: "Premier projet durant ma formation en BUT Informatique, cette <strong>application terminal</strong> " +
+        "permet à deux joueurs de se défier sur une <strong>partie de Yams</strong>, célèbre jeu de 5 dés.",
       competences: [
         "extraire des besoins",
         "implémenter",
@@ -31,10 +31,10 @@ export default function SectionProjets() {
       dates: "Novembre 2021 - Février 2022",
       equipe: true,
       texteEquipe: "Concours entrepreneurial en équipe",
-      description: "Dans le cadre du <b>concours entrepreneurial</b>, et par groupe de jeunes étudiants, nous " +
-        "avions 3 mois pour trouver une <b>idée d'entreprise innovante</b> et la développer. " +
-        "Notre équipe a eu pour idée de créer une <b>application anti-tabac</b>, Fuzway, dont l'innovation " +
-        "reposait sur la <b>communication</b> entre fumeurs et sur <b>l'esprit d'équipe</b>.",
+      description: "Dans le cadre du <strong>concours entrepreneurial</strong>, et par groupe de jeunes étudiants, nous " +
+        "avions 3 mois pour trouver une <strong>idée d'entreprise innovante</strong> et la développer. " +
+        "Notre équipe a eu pour idée de créer une <strong>application anti-tabac</strong>, Fuzway, dont l'innovation " +
+        "reposait sur la <strong>communication</strong> entre fumeurs et sur <strong>l'esprit d'équipe</strong>.",
       competences: [
         "travailler en équipe",
         "faire preuve de créativité",
@@ -51,8 +51,8 @@ export default function SectionProjets() {
       temps: 120,
       equipe: true,
       description: "KohFrais, une entreprise de vente de produits surgelés et de glaces, souhaitait " +
-        "disposer d'un <b>site vitrine</b> pour se faire connaître. En équipe, nous avons <b>extrait leurs " +
-        "besoins</b>, imaginé l'arborescence du site, créé la <b>maquette</b> et la <b>charte graphique</b>, " +
+        "disposer d'un <strong>site vitrine</strong> pour se faire connaître. En équipe, nous avons <strong>extrait leurs " +
+        "besoins</strong>, imaginé l'arborescence du site, créé la <strong>maquette</strong> et la <strong>charte graphique</strong>, " +
         "puis développé le site web.",
       competences: [
         "extraire des besoins",
--- components/Projets/Projet.jsx
+++ components/Projets/Projet.jsx
@@ -1,21 +1,70 @@
+import Image from 'next/image';
 import PropTypes from 'prop-types'
 import { useInView } from 'react-intersection-observer'
+import { CalendarEvent, Group, Time, User } from '../Icons';
 
 function Projet(props) {
   // Définit si le bloc est visible à 10%
-  const {ref, inView } = useInView({threshold: 0.1});
+  const { ref, inView } = useInView({ threshold: 0.1 });
+  const Icon = props.icon;
+
+  // Définition du texte affiché à côté de l'équipe
+  let texteEquipe;
+  if (props.texteEquipe !== undefined) texteEquipe = props.texteEquipe;
+  else if (props.equipe) texteEquipe = "Projet réalisé en équipe"
+  else texteEquipe = "Projet réalisé seul"
 
   return (
-    <div ref={ref} className={"h-[90vh] min-w-full snap-center " + props.fond + " " + props.texte}>
-      
-    </div>
+    <article ref={ref} className={"h-[90vh] min-w-full snap-center px-28 py-14 " + props.fond + " " + props.texte}>
+      <h2 className='text-4xl text-center font-bold mb-8'>
+        <Icon className={"inline w-16 h-16 align-middle mb-2 mr-7 " + props.texte} />
+        {props.titre}
+      </h2>
+
+      <div className='flex text-lg items-start'>
+        <div className='shrink-0 mr-12'>
+          <Image src={props.image} quality={100} className="self-center" />
+        </div>
+
+        <div>
+          <div className='flex flex-wrap gap-x-12 gap-y-3 mb-3'>
+            <p className='flex items-center gap-3 shrink-0'>
+              <CalendarEvent className={props.texte} />
+              {props.dates}
+            </p>
+
+            {
+              props.temps !== undefined && <p className='flex items-center gap-3 shrink-0'>
+                <Time className={props.texte} />
+                {props.temps}h
+              </p>
+            }
+          </div>
+
+          <p className='flex items-center gap-3 mb-12'>
+            {props.equipe ? <Group className={props.texte} /> : <User className={props.texte} />}
+            {texteEquipe}
+          </p>
+
+          <h3 className='text-2xl font-bold mb-5'>Description</h3>
+          <p className='mb-10' dangerouslySetInnerHTML={{__html: props.description}}></p>
+
+          <p><strong>Compétences :</strong> {props.competences.join(", ")}</p>
+        </div>
+      </div>
+
+    </article>
   )
+}
+
+Projet.defaultProps = {
+  equipe: false
 }
 
 Projet.propTypes = {
   titre: PropTypes.string.isRequired,
   icon: PropTypes.func.isRequired,
-  image: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
   /** Définition des couleurs */
   fond: PropTypes.string,
   texte: PropTypes.string,
