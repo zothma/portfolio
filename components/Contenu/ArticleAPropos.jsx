e1b6bfce0f3e281f8ddee9942aab0f61184ff7c1
Enzo MAROS <enzo.maros@gmail.com>
2023-10-16 16:28:35 +0200

    Mise à jour age

--- components/Contenu/ArticleAPropos.jsx
+++ components/Contenu/ArticleAPropos.jsx
@@ -6,7 +6,7 @@ export default function ArticleAPropos() {
     <Article titre="À propos" id="présentation">
       <p className="text-center leading-loose mb-12">
         <strong>Passionné</strong> par l&apos;informatique depuis l&apos;âge de 8 ans, j&apos;ai très vite commencé
-        le <strong>développement</strong> Web et Applications. Aujourd&apos;hui âgé de 18 ans, cet enthousiasme pour
+        le <strong>développement</strong> Web et Applications. Aujourd&apos;hui âgé de 20 ans, cet enthousiasme pour
         la <strong>programmation</strong> ne m&apos;a pas quitté, et je compte bien en faire mon <strong>métier</strong>.
       </p>
       <p className="text-center">
