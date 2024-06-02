da09896352160eb94ec84e9950403f7c9b44e6b0
Enzo MAROS <enzo.maros@gmail.com>
2024-06-02 15:30:27 +0200

    [FIX] Nouvelle URL opengraph

--- pages/_document.js
+++ pages/_document.js
@@ -10,16 +10,16 @@ export default function Document() {
         <meta name="theme-color" content="#4D4B44" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://enzo-maros.fr/" />
+        <meta property="og:url" content="https://enzo-maros.com/" />
         <meta property="og:title" content="Enzo MAROS - Portfolio" />
         <meta property="og:description" content="Étudiant à l'IUT de Lannion, je suis en formation BUT Informatique afin de devenir à terme développeur." />
-        <meta property="og:image" content="https://enzo-maros.fr//assets/open-graph.png" />
+        <meta property="og:image" content="https://enzo-maros.com//assets/open-graph.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://enzo-maros.fr/" />
+        <meta property="twitter:url" content="https://enzo-maros.com/" />
         <meta property="twitter:title" content="Enzo MAROS - Portfolio" />
         <meta property="twitter:description" content="Étudiant à l'IUT de Lannion, je suis en formation BUT Informatique afin de devenir à terme développeur." />
-        <meta property="twitter:image" content="https://enzo-maros.fr//assets/open-graph.png" />
+        <meta property="twitter:image" content="https://enzo-maros.com//assets/open-graph.png" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
