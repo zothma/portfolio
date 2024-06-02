493281ec60980c416f25a9b43fea14168cca066e
Enzo MAROS <enzo.maros@gmail.com>
2024-06-02 17:32:19 +0200

    Refactorisation Compétences

--- components/CarteCompetences.jsx
+++ components/CarteCompetences.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import Image from 'next/image'
 
-function genererIcone({ icon, alt }) {
+function Competence({ icon, alt }) {
   return (
     <div title={alt} className="relative flex items-center w-16 h-16" key={alt}>
       <Image src={"/icons/logos/" + icon} alt={alt} layout="fill" objectFit='contain' />
@@ -11,16 +11,17 @@ function genererIcone({ icon, alt }) {
 
 export default function CarteCompetences(props) {
   // Le premier mot apparaît en orange, le reste en gris
-  const [mot1, ...mots] = props.titre.split(" ")
+  const [premierMot, ...mots] = props.titre.split(" ")
 
   return (
     <div className='relative bg-[#fff] p-8 rounded-3xl flex-grow flex-shrink basis-0 shadow-lg'>
       <h3 className='font-ubuntu font-bold text-3xl mb-8'>
-        <span className='text-orange'>{mot1} </span>
+        <span className='text-orange'>{premierMot} </span>
         {mots}
       </h3>
+
       <div className='flex flex-wrap gap-10 justify-center'>
-        {props.data.map(genererIcone)}
+        {props.data.map(props => <Competence {...props} />)}
       </div>
     </div>
   )
--- components/Contenu/ArticleCompetences.jsx
+++ components/Contenu/ArticleCompetences.jsx
@@ -2,54 +2,56 @@ import CarteCompetences from "../CarteCompetences";
 
 import Article from './Article';
 
-const webFrontData = [
-  {icon: "HTML5.svg", alt: "HTML5"},
-  {icon: "CSS3.svg", alt: "CSS3"},
-  {icon: "javascript.svg", alt: "JavaScript"},
-  {icon: "bootstrap.svg", alt: "Bootstrap"},
-];
-
-const webBackData = [
-  {icon: "PHP.svg", alt: "PHP"},
-  {icon: "nodejs.svg", alt: "NodeJS"},
-  {icon: "postgresql.svg", alt: "PostgreSQL"},
-];
-
-const logicielData = [
-  {icon: "C.svg", alt: "Langage C"},
-  {icon: "Java.svg", alt: "Java"},
-  {icon: "python.svg", alt: "Python"},
-  {icon: "dartlang.svg", alt: "Dart"},
-];
-
-const outilsData = [
-  {icon: "github.svg", alt: "GitHub"},
-  {icon: "Git.svg", alt: "Git"},
-  {icon: "Figma.svg", alt: "Figma"},
-  {icon: "VSC.svg", alt: "Éditeur Visual Studio Code"},
-  {icon: "android-studio.svg", alt: "Android Studio"},
-];
-
-const systemData = [
-  {icon: "Windows.svg", alt: "Windows"},
-  {icon: "Linux.svg", alt: "Linux"},
-  {icon: "Ubuntu.svg", alt: "Ubuntu"},
-  {icon: "archlinux.svg", alt: "Arch Linux"},
-  {icon: "docker.svg", alt: "Docker"},
-]
-
+/**
+ * Contenu de la section "Compétences".
+ */
 export default function ArticleCompetences() {
   return (
     <Article id="competences" titre="Compétences" >
       <div className="flex flex-col gap-8">
         <div className="flex flex-col lg:flex-row gap-8">
-          <CarteCompetences titre="Web Front" data={webFrontData} />
-          <CarteCompetences titre="Web Back" data={webBackData} />
-          <CarteCompetences titre="Logiciel" data={logicielData} />
+          {/* WebFront */}
+          <CarteCompetences titre="Web Front" data={[
+            {icon: "HTML5.svg", alt: "HTML5"},
+            {icon: "CSS3.svg", alt: "CSS3"},
+            {icon: "javascript.svg", alt: "JavaScript"},
+            {icon: "bootstrap.svg", alt: "Bootstrap"},
+          ]} />
+
+          {/* WebBack */}
+          <CarteCompetences titre="Web Back" data={[
+            {icon: "PHP.svg", alt: "PHP"},
+            {icon: "nodejs.svg", alt: "NodeJS"},
+            {icon: "postgresql.svg", alt: "PostgreSQL"},
+          ]} />
+
+          {/* Logiciel */}
+          <CarteCompetences titre="Logiciel" data={[
+            {icon: "C.svg", alt: "Langage C"},
+            {icon: "Java.svg", alt: "Java"},
+            {icon: "python.svg", alt: "Python"},
+            {icon: "dartlang.svg", alt: "Dart"},
+          ]} />
         </div>
+
         <div className="flex flex-col lg:flex-row gap-8">
-          <CarteCompetences titre="Outils" data={outilsData} />
-          <CarteCompetences titre="Systèmes" data={systemData} />
+          {/* Outils */}
+          <CarteCompetences titre="Outils" data={[
+            {icon: "github.svg", alt: "GitHub"},
+            {icon: "Git.svg", alt: "Git"},
+            {icon: "Figma.svg", alt: "Figma"},
+            {icon: "VSC.svg", alt: "Éditeur Visual Studio Code"},
+            {icon: "android-studio.svg", alt: "Android Studio"},
+          ]} />
+
+          {/* Systèmes */}
+          <CarteCompetences titre="Systèmes" data={[
+            {icon: "Windows.svg", alt: "Windows"},
+            {icon: "Linux.svg", alt: "Linux"},
+            {icon: "Ubuntu.svg", alt: "Ubuntu"},
+            {icon: "archlinux.svg", alt: "Arch Linux"},
+            {icon: "docker.svg", alt: "Docker"},
+          ]} />
         </div>
       </div>
     </Article>
